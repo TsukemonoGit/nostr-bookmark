2efb899ac3bb7c719227f4996c78037b5d86cc49
TsukemonoGit <mine43try.outlook.com>
2023-04-02 14:23:24 +0900

    bi

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,108 +5,118 @@
 
     import { bech32encode } from "../lib/bech32";
 
-    
     /**
      * @type {string | any[]}
      */
-    let bookmarkList;
+    let bookmarkList=[];
 
-    let relayName = "";//"wss://nostream.localtest.me/"; //"wss://relay.damus.io";
-    let copyRelayName = "";//"wss://nostream.localtest.me/";
+    let relayName = "wss://nostream.localtest.me/"; //"wss://relay.damus.io";
+    let copyRelayName = ""; //"wss://nostream.localtest.me/";
     let connectMessage = "";
     let connectMessage2 = "";
-    let pubkey ="";
-       // "npub12egp0pvh2f0fp6sk5nt6ncehqzkz8zsma8dl8agf8p3f98v6resqku4w26";
+    let pubkey = "";
+    let errorMessage = "";
+    let errorMessage2 = "";
+
+    // "npub12egp0pvh2f0fp6sk5nt6ncehqzkz8zsma8dl8agf8p3f98v6resqku4w26";
     //"npub1sjcvg64knxkrt6ev52rywzu9uzqakgy8ehhk8yezxmpewsthst6sw3jqcw";
     let author = "";
-    let bookmark1_length=0;
-    let isView=false;
+    let bookmark1_length = 0;
+    let isView = false;
     async function clickButton() {
-        isView=false;
-        bookmark1_length=0;
+        isView = false; //一旦結果表示の部分非表示に
+        bookmark1_length = 0;
+        errorMessage = "";
+        errorMessage2="";
         bookmarkList = [];
         connectMessage = "通信中";
         relayName = relayName.trim(); //空白除去
         let str = relayName.slice(0, 6); //始まりがwss://かチェック
 
-        if (str !== "wss://") {
-            alert("wss://〜〜を入力してください");
+        const relay = relayInit(relayName);
+
+        const connect_obj = await ConnectRelay(relay);
+        if (!connect_obj.success) {
+            errorMessage = connect_obj.message;
             return;
-        } else {
-            const relay = relayInit(relayName);
-            try {
-                const isConnect = await ConnectRelay(relay);
-                if (!isConnect) {
-                    connectMessage = `failed to connect to ${relayName}`;
-                    return;
-                } else {
-                    //-----以下コネクト成功-------
-                    //------ブクマ取得＆表示------
-                    connectMessage = `connected to ${relayName}`;
-
-                    author = pubkey;
-
-                    if (pubkey.slice(0, 4) === "npub") {
-                        console.log("バブキーヘックスに変換");
-                        try {
-                            author = bech32encode(pubkey);
-                        } catch (error) {
-                            connectMessage = "公開鍵が正しいか確認してください";
-                            return;
-                        }
-                    }
-                }
-            } catch (error) {
-                connectMessage = `failed to connect to ${relayName}`;
-                return;
-            }
-
-            let sub = relay.sub([
-                {
-                    kinds: [30001], //ブックマーク 30001
-                    authors: [author],
-                },
-            ]);
-            
-            bookmarkList = await listenForEvent();
-            async function listenForEvent() {
-                return new Promise((resolve) => {
-                    isView=true;
-                    sub.on("event", (event) => {
-                        bookmark1_length=event.tags.length-1;
-                       if(bookmark1_length<0)bookmark1_length=0;
-                        resolve(event.tags);
-                        //connectMessage="";
-                        
-                    });
-                    sub.on("eose",()=>{
-                       if(bookmark1_length<=0)bookmark1_length=0;
-                       // connectMessage="ブクマ保存されてないっぽい！"
-                       
-                    })
-                });
-            }
-            console.log(`bookmark:${bookmarkList}`);
-           
-
-            // `bookmarkList`を使って、必要な処理を行う
         }
+        //-----以下コネクト成功-------
+        //------ブクマ取得＆表示------
+        connectMessage = connect_obj.message;
+        author=pubkey;
+        if ( pubkey.slice(0, 4) === "npub") {
+            const pub_obj = CreateHex(author);
+            if (!pub_obj.isSuccess) {
+                errorMessage2 = pub_obj.connectMessage;
+                return;
+            } else{author=pub_obj.author;}
+        }
+
+        bookmarkList = await listenForEvent(relay);
+        
+        console.log(`bookmark:${bookmarkList}`);
+
+        // `bookmarkList`を使って、必要な処理を行う
     }
 
+    /**
+     * @param {import("nostr-tools").Relay} relay
+     */
+    async function listenForEvent(relay) {
+        let sub = relay.sub([
+            {
+                kinds: [30001], //ブックマーク 30001
+                authors: [author],
+            },
+        ]);
+            return new Promise((resolve) => {
+                isView = true;
+                sub.on("event", (event) => {
+                    bookmark1_length = event.tags.length - 1;
+                    if (bookmark1_length < 0) {
+                        bookmark1_length = 0;
+                        errorMessage2="ブクマ保存されてないっぽい！";
+                    }
+                    resolve(event.tags);
+                    
+                    //connectMessage="";
+                });
+                sub.on("eose", () => {
+                    if (bookmark1_length <= 0) {
+                        bookmark1_length = 0;
+                        errorMessage2="ブクマ保存されてないっぽい！";
+                    
+                    }
+                    // connectMessage="ブクマ保存されてないっぽい！"
+                });
+            });
+        }
     // @ts-ignore
     async function ConnectRelay(relay) {
         let bool = false;
-        relay.on("connect", () => {
-            console.log(`connected to ${relay.url}`);
-            bool = true;
-        });
-        relay.on("error", () => {
-            console.log(`failed to connect to ${relay.url}`);
-            bool = false;
-        });
+        let message = "";
+        try {
+            relay.on("connect", () => {
+                bool = true;
+                message = `connected to ${relay.url}`;
+                console.log(message);
+            });
+            relay.on("error", () => {
+                bool = false;
+                message = `failed to connect to ${relay.url}`;
+                console.log(message);
+            });
 
-        await relay.connect();
-        return bool;
+            await relay.connect();
+        } catch (error) {
+            bool = false;
+            message = `リレーが見つかりません。リレーアドレスを確認してください`;
+            console.log(message);
+        }
+        return {
+            success: bool,
+            message: message,
+        };
     }
 
     //------------------------------------------------
@@ -146,116 +156,144 @@
             });
         }
     }
+
+    /**
+     * @param {string} pubkey
+     */
+    function CreateHex(pubkey) {
+        let isSuccess = false;
+        let connectMessage = "";
+
+        console.log("バブキーヘックスに変換");
+        try {
+            author = bech32encode(pubkey);
+            isSuccess = true;
+        } catch (error) {
+            isSuccess = false;
+            connectMessage = "公開鍵が正しいか確認してください";
+        }
+
+        return { isSuccess, author, connectMessage };
+    }
 </script>
 
 <!-------------------------------------------------------------------->
 <main>
-<h2>消えたブックマークを取り戻せ</h2>
-<p>kind:30001に保存されている公開ブックマークをリレーからリレーに移植</p>
-<ul>
-    <li>自分の公開鍵、ブックマークが残っていそうなリレーを入力</li>
-    <li>
-        拡散したい（上書きしたい）リレーを選択して、保存<br />
-        署名するためにnip-7拡張機能を使います<br />
-    </li>
+    <h2>消えたブックマークを取り戻せ</h2>
+    <p>kind:30001に保存されている公開ブックマークをリレーからリレーに移植</p>
+    <ul>
+        <li>自分の公開鍵、ブックマークが残っていそうなリレーを入力</li>
+        <li>
+            拡散したい（上書きしたい）リレーを選択して、保存<br />
+            署名するためにnip-7拡張機能を使います<br />
+        </li>
     </ul>
 
-<!-------------------------------------------------------------------->
-<div class = "content">
-    pubkey:
-    <input
-        type="text"
-        bind:value={pubkey}
-        placeholder="npub or hex"
-        style="min-width:600px"
-    />
-
-    relay:
-    <input
-        type="text"
-        bind:value={relayName}
-        placeholder="wss://..."
-        style="min-width:350px"
-    />
-
-   
-<button on:click={clickButton}> BookmarkListを取得</button>
-</div>
-<hr/>
-<p>{connectMessage}</p>
-<!-------------------------------------------------------------------->
-<!--{#if bookmarkList != null}-->
-{#if !isView}
-<div id="bookmarkList"/>
-{/if}
-{#if isView}
-<div id = "bookmarkList">  
-<p>
-        ブックマーク件数:{bookmark1_length}
-    </p>
-  
-     <!-------------------------------------------------------------------->
-     <details>
-        <summary>イベントIDリスト</summary>
-        <ul class="bcmList">
-            {#each bookmarkList.slice(1)  as bookmark}
-                <li>{bookmark[1]}</li>
-            {/each}
-        </ul>
-    </details>
-    
-    <!----別のリレーへ-------------------------------------------------------->
-    <hr/>
-    <div class = "content">
-        to relay:
+    <!-------------------------------------------------------------------->
+    <div class="content">
+        pubkey:
         <input
             type="text"
-            bind:value={copyRelayName}
-            placeholder="wss://..."
-            style="min-width:250px"
+            bind:value={pubkey}
+            placeholder="npub or hex"
+            style="min-width:600px"
         />
+
+        relay:
+        <input
+            type="text"
+            bind:value={relayName}
+            placeholder="wss://..."
+            style="min-width:350px"
+        />
+
+        <button on:click={clickButton}> BookmarkListを取得</button>
     </div>
-    <button on:click={clickCopyButton}>
-        {copyRelayName}に上書き保存</button>
-        {#if connectMessage2.length==0}
-        <div style="margin:50px"/>
-        {/if}
-    <p>{connectMessage2}</p>
-   <hr/>
-</div>
+    <hr />
+    {#if errorMessage.length > 0}
+        <div class="error-message">{errorMessage}</div>
+    {/if}
+    {#if errorMessage.length == 0}
+        <div class="message">{connectMessage}</div>
+    {/if}
+    {#if errorMessage2.length > 0}
+        <div class="error-message">{errorMessage2}</div>
+    {/if}
+    <!-------------------------------------------------------------------->
+    <!--{#if bookmarkList != null}-->
+    {#if !isView}
+        <div id="bookmarkList" />
+    {/if}
+    {#if isView }
+        <div id="bookmarkList">
+            <p>
+                ブックマーク件数:{bookmark1_length}
+            </p>
 
+            <!-------------------------------------------------------------------->
+            {#if bookmark1_length>0}
+            <details>
+                <summary>イベントIDリスト</summary>
+                <ul class="bcmList">
+                    {#each bookmarkList.slice(1) as bookmark}
+                        <li>{bookmark[1]}</li>
+                    {/each}
+                </ul>
+            </details>
 
-{/if}
-
+            <!----別のリレーへ-------------------------------------------------------->
+            <hr />
+            <div class="content">
+                to relay:
+                <input
+                    type="text"
+                    bind:value={copyRelayName}
+                    placeholder="wss://..."
+                    style="min-width:250px"
+                />
+            </div>
+            <button on:click={clickCopyButton}>
+                {copyRelayName}に上書き保存</button
+            >
+            {#if connectMessage2.length == 0}
+                <div style="margin:50px" />
+            {/if}
+            <p>{connectMessage2}</p>
+            <hr />
+            {/if}
+        </div>
+  
+    {/if}
 </main>
+
 <!---------------------------------------------------------->
 <style>
+    .error-message {
+        color: red;
+    }
     hr {
         margin: 20px 0;
-      }
-  
-    
-button{
-    font-size:medium;
-    display: block;
-    padding: 10px;
-    margin: 20px 5px 20px 5px;
-    border: solid 1px lightgray;
-    cursor: pointer;
-}
-.content {
-    margin-top: 10px;
-    margin-left: 10px;
-    white-space: pre-line;
-  }
-  input{
-    font-size: medium;
-    padding:10px 10px 5px 10px ;
-    margin:5px;
-  }
-  .bcmList{
-    list-style:decimal-leading-zero;
-    
     }
-   
-</style> 
+
+    button {
+        font-size: medium;
+        display: block;
+        padding: 10px;
+        margin: 20px 5px 20px 5px;
+        border: solid 1px lightgray;
+        cursor: pointer;
+    }
+    .content {
+        margin-top: 10px;
+        margin-left: 10px;
+        white-space: pre-line;
+    }
+    input {
+        font-size: medium;
+        padding: 10px 10px 5px 10px;
+        margin: 5px;
+    }
+    .bcmList {
+        list-style: decimal-leading-zero;
+    }
+</style>
